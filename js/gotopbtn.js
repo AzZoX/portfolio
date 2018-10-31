d7ad6afeb71f8519e5f51b0497bf1c4f9ef93995
AzZoX <skytyu22@gmail.com>
2018-10-31 14:14:44 +0100

    Amélioration gotopbtn.

---
+++ js/gotopbtn.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	$(window).scroll(function(){
+
+		if ($(this).scrollTop() > 40) {
+			$(".gotopbtn").fadeIn();
+		}
+		else {
+			$(".gotopbtn").fadeOut();
+		}
+		
+	});
+});
