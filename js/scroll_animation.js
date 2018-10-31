f82644def6e5f7df0086600d2b3c3f2621f8a40f
AzZoX <skytyu22@gmail.com>
2018-10-31 18:21:42 +0100

    Modification de scroll_bar par scroll_categories.

--- js/scroll_animation.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-
-	$(window).scroll(function(){
-
-		var topValue = ($(this).scrollTop() / $(document).height()) * $(".scroll_animation").height();
-		$(".scroll_bar").css("top", topValue);
-	});
-});
---
+++ js/scroll_categories.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+	var current_clicked_id = "scroll_cat1";
+
+	function setDarkDot(id) {
+		current_clicked_id = id;
+		$(".scroll_categories > a > i").removeClass("fas fa-dot-circle"); //Supprime le point noir de tous les cat.
+		$("#" + id).find("i").addClass("fas fa-circle"); //Ajout du point noir sur la premiere cat.
+	}
+
+	// Scroll Boutons
+	$(".scroll_categories > a").mouseover(function(){
+		if (current_clicked_id != this.id) {
+			$(this).find("i").removeClass("far fa-circle").addClass("far fa-dot-circle");
+		}
+	});
+
+	$(".scroll_categories > a").mouseout(function(){
+		$(this).find("i").removeClass("far fa-dot-circle").addClass("far fa-circle");
+	});
+
+	$(".scroll_categories > a").click(function(){
+		current_clicked_id = this.id;
+		$(this).find("i").removeClass("far fa-dot-circle fa-circle").addClass("fas fa-circle");
+
+		$(this).siblings().find("i").removeClass("fas fa-dot-circle").addClass("far fa-circle"); //Supprime le point noir pour les autres cat.
+	});
+
+	// Bouton go top
+	$(".gotopbtn").click(function(){
+		setDarkDot("scroll_cat1");
+	});
+
+	// Liens navbar
+	$(".nav-link").click(function(){
+		if (this.text == "Cat1") {
+			setDarkDot("scroll_cat1");
+		}
+		else if (this.text == "Cat2") {
+			setDarkDot("scroll_cat2");
+		}
+		else if (this.text == "Cat3") {
+			setDarkDot("scroll_cat3");
+		}
+	});
+});
